8457210f318057e807bd7926046a6db68f3b22aa
Troys-Code <41653822+Troys-Code@users.noreply.github.com>
2021-03-03 12:23:42 +0900

    Delete Python_Projects directory

--- Python_Projects/[Python]DesertedIslandGame/file_handler.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-def open_file(path):        # For Path a file name in the same folder will do
-    os.system("open " + path)
--- Python_Projects/[Python]Quest_Tasker.py/quest.py
+++
@@ -1,7 +0,0 @@
-class AddInfo():
-
-    def __init__(self, course, homework, due, est):
-        self.course = course        #Class Name
-        self.hw = homework          #Homework Item
-        self.duedate = due          #Due Date
-        self.dedication = est       #Estimated Time Dedication Until Completion
