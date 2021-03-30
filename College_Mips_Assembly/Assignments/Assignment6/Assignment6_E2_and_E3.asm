132290ba8bc44fd2cdd8c163bbec879d6eacff4e
Troys-Code <41653822+Troys-Code@users.noreply.github.com>
2021-03-30 12:07:20 +0900

    Update Assignment6_E2_and_E3.asm
    Cleaned up to simplify solution and comments, goal was to use less nop instructions and make the solution easier to read.

--- College_Mips_Assembly/Assignments/Assignment6/Assignment6_E2_and_E3.asm
+++ College_Mips_Assembly/Assignments/Assignment6/Assignment6_E2_and_E3.asm
@@ -16,59 +16,53 @@
 # Using $t4 as 'a': Top Of Range to be converted to lowercase
 # Using $t5 as 'z': End of Range to be converted to lowercase
 #	
-	.data
-string:	.asciiz	"in   a hole   in the ground there lived a hobbit"
-newln:	.asciiz	"\n"
+.data	
+string:	.asciiz "in a hole in the ground there lived a hobbit"
+newline: .asciiz "\n"
+
 	.text
-	.globl	main
-	
+	.globl main
+
 main:
-	li	$v0, 4			# 
-	la	$a0, string		# Print the original string
-	syscall				# 
+	li	$v0, 4			 
+	la	$a0, string			# Print string
+	syscall				 
 
-	li	$t0, 0			# $t0 = index
-	li	$t1, ' '		# $t1 = space
-	li	$t2, ' '		# $t2 = prev char : initialized as space
-	li	$t4, 'a'		# $t4 = a: Top Of interested Range 
-	li	$t5, 'z'		# $t5 = z: End of interested Range
-	
+	li	$t0, 0				# Index
+	li	$t1, ' '			# Previous = space
 while:
-	lb	$t3, string($t0)	# Current char : $t3 = string[index] 
 	nop
-	beqz	$t3, exit		# If (current char == NULL) {Exit loop}
+	lb	$t2, string($t0)		# Current = string[Index] 
 	nop
-	bne	$t2, $t1, continue	# If (prev char != ' ') {continue loop}  
+	beqz	$t2, End			# if ( Current == NULL ) { End loop }
+	bne	$t1, ' ', Next			# if ( Previous != space ) { Next }			
+	bge	$t2, 'a', a_			# If ('a'- ?) { check if 'a' - 'z' }
+Next:
 	nop
-	bge	$t3, $t4, topRange	# If (currentchar >= 'a') {j topRange}
+	move	$t1, $t2			# Previous = Next
+	addi	$t0, 1				# Index++
+	j	while				# Continue looping
+a_:	
+	nop			
+	ble	$t2, 'z', a_z			# if (Next <= 'z') {Capitalize} 
+	j	Next				# else (not a-z) {continue looping}
+a_z:	
+	nop					# Is a-z
+	addiu	$t2, -0x20			# uppercase
+	sb	$t2, string($t0)		# string[i] = 'uppercase'
 	nop
-continue:
-	move	$t2, $t3		# $t2 <- $t3 : prev char = current char
-	addi	$t0, 1			# Increment offset $t0 + 1
-	j	while			# Return to the top of while loop
+	j	Next				# Continue loop
+End:
 	nop
-topRange:				# Current char is Greater than 'a'
-	ble	$t3, $t5, inRange	# If current char <= 'z' {in range a-z}
-	nop
-	j	continue		# If (not a-z) {continue loop}
-	nop
-inRange:				# Since its in Range a-z
-	addiu	$t3, -0x20		# Conv currentchar in $t3 to uppercase
-	sb	$t3, string($t0)	# Store uppercase char in string
-	nop
-	j	continue		# Continue loop
-	nop
-exit:
+	li	$v0, 4
+	la	$a0, newline			# Print a new line character
+	syscall				
 
-	li	$v0, 4			#
-	la	$a0, newln		# Print a new line character
-	syscall				#
+	li	$v0, 4			
+	la	$a0, string			# Print New String
+	syscall	
 
-	li	$v0, 4			#
-	la	$a0, string		# Print converted to lowercase string
-	syscall				#
-
-	li	$v0, 10			# Code "10" to end program
+	li	$v0, 10				# Exit Program
 	syscall			
 	
 ## End of file
